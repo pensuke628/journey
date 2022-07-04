59bbefefcb89347f4c3260343edc7269e3a5caf2
pensuke628 <ryz123212327@gmail.com>
2022-07-04 19:10:47 +0900

    [back][feat]:carrierwaveの保存先を本番環境でS3を用いるための設定を追加した

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -1,5 +1,18 @@
 CarrierWave.configure do |config|
-  config.asset_host = 'http://localhost:3010'
-  config.storage = :file
-  config.cache_storage = :file
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = 'journey-s3'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
+      path_style: true
+    }
+  else
+    config.asset_host = 'http://localhost:3010'
+    config.storage = :file
+    config.cache_storage = :file
+  end
 end
