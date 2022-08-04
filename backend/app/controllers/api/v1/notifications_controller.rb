709672e932bc507a5338cdb254ee962f41f6bc9d
pensuke628 <ryz123212327@gmail.com>
2022-08-04 22:30:43 +0900

    [back][refactor]:口コミを既読にするためのルーティングとアクションを定義した

--- backend/app/controllers/api/v1/notifications_controller.rb
+++ backend/app/controllers/api/v1/notifications_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::NotificationsController < ApplicationController
   def index
     render json: {
       status: 200,
-      notifications: current_api_v1_user.passive_notifications.order_new
-      # 自分宛の通知を日付が新しい順で返す
+      notifications: current_api_v1_user.passive_notifications.order_new.limit(9)
+      # 自分宛の通知を日付が新しい順で最大9件返す
     }
   end
 
@@ -31,9 +31,35 @@ class Api::V1::NotificationsController < ApplicationController
     render json: { status: 200 }
   end
 
+  def checked_notification
+    current_user = User.find(params[:id])
+    notifications = current_user.passive_notifications.unread_only
+    notifications.each do |notification|
+      notification.update!(checked: true)
+    end
+    if current_user.passive_notifications.unread_only.count.zero?
+      render json: { status: 200 }
+    else
+      render json: { status: :unprocessable_entity }
+    end
+  end
+
+  # デバッグ用
+  def reset_notification
+    current_user = User.find(1)
+    notifications = current_user.passive_notifications
+    notifications.each do |notification|
+      notification.update!(checked: false)
+    end
+  end
+
   private
 
     def notification_params
       params.permit(:sender_id, :receiver_id, :review_id, :comment_id, :message_id, :act)
+    end
+
+    def notification_update_params
+      params.permit(:id, :checked)
     end
 end
